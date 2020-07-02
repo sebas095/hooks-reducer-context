70cc75ee3965de884bece75975449e89205e76cd
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-07-02 02:52:23 -0500

    ADD - counter reducer added

---
+++ src/store/reducers/counter.reducer.js
@@ -0,0 +1,14 @@
+import { DECREMENT, INCREMENT } from "../types";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
