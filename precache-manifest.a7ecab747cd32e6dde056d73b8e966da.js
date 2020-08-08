a59756473abde3abc4a77fc99c6a32b3ee494d21
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-08-07 22:32:45 -0500

    Updates

---
+++ precache-manifest.a7ecab747cd32e6dde056d73b8e966da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24cf717d91cd1906a7aef216ac3f753",
+    "url": "/hooks-reducer-context/index.html"
+  },
+  {
+    "revision": "fd9d2d9271e6793f16e9",
+    "url": "/hooks-reducer-context/static/js/2.d4db7dae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hooks-reducer-context/static/js/2.d4db7dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a765745bba855229b03c",
+    "url": "/hooks-reducer-context/static/js/main.4e02ace1.chunk.js"
+  },
+  {
+    "revision": "e32eba92be4b10d67db2",
+    "url": "/hooks-reducer-context/static/js/runtime-main.e0d7b447.js"
+  },
+  {
+    "revision": "ef6f7de594e105cadcec3c6440a61eae",
+    "url": "/hooks-reducer-context/static/media/chrono.ef6f7de5.png"
+  },
+  {
+    "revision": "bb1e80ec99462f116e2ac5f755623884",
+    "url": "/hooks-reducer-context/static/media/react-hooks.bb1e80ec.png"
+  }
+]);
