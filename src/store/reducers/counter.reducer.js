ffd7268ff8b6029746d2b2f02594b7e17b2ba140
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-08-07 22:23:55 -0500

    REF - reducers refactored

--- src/store/reducers/counter.reducer.js
+++ src/store/reducers/counter.reducer.js
@@ -1,19 +1,17 @@
 import { DECREMENT, INCREMENT } from "../types";
 
 const reducer = (state, action) => {
-  const { counter } = state;
-
   switch (action.type) {
     case INCREMENT:
       return {
         ...state,
-        counter: { ...counter, count: counter.count + 1 },
+        count: state.count + 1,
       };
 
     case DECREMENT:
       return {
         ...state,
-        counter: { ...counter, count: counter.count - 1 },
+        count: state.count - 1,
       };
 
     default:
